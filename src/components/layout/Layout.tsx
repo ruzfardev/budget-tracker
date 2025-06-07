f333244eaeb515a69eb41e1ea9fdce4fbc3de6de
ruzfardev <farrukhruzmetov2002@gmail.com>
2025-06-07 16:39:14 +0500

    refactor: comment out OfflineIndicator in Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ import { OfflineIndicator } from '../common'
 const Layout = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-white to-blue-50 dark:from-gray-900 dark:via-gray-900 dark:to-gray-800">
-      <OfflineIndicator />
+      {/* <OfflineIndicator /> */}
       <Header />
       <div className="flex">
         <Navigation />
